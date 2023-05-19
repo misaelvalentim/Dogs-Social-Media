19400c4bd37b320c030d1e938fd0ca877ba19689
Misael Valentim <misael.valentim@valtech.com>
2023-05-19 12:04:17 -0300

    Setting up post photos feature

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -4,6 +4,10 @@ const validation = {
     email: {
         regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
         message: 'Fill out a valid email'
+    },
+    number: {
+        regex: /^\d+$/,
+        message: 'Use only numbers.'
     }
 }
 
