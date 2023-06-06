3b675fe4391e5cac53563e3ac3dbb835ad312a2a
Misael Valentim <misael.valentim@valtech.com>
2023-06-06 17:14:40 -0300

    reseting password feature

--- src/Components/Login/ForgotPassword.js
+++ src/Components/Login/ForgotPassword.js
@@ -1,10 +1,42 @@
 import React from 'react'
+import Input from '../Forms/Input'
+import Button from '../Forms/Button'
+import useForm from '../../Hooks/useForm'
+import useFetch from '../../Hooks/useFetch'
+import { PASSWORD_LOST } from '../../api'
+import Error from '../Helper/Error'
 
 const ForgotPassword = () => {
+    const login = useForm();
+    const { data, loading, error, request } = useFetch();
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        if (login.validate()) {
+            const { url, options } = PASSWORD_LOST(
+                {
+                    login: login.value,
+                    url: window.location.href.replace('forgotpassword', 'resetpassword')
+                }
+            );
+            const { json } = await request(url, options)
+            console.log(json);
+        }
+    }
+
     return (
-        <div>
-            <h1>Forgot password</h1>
-        </div>
+        <section>
+            <h1>Forgot your password?</h1>
+            {data ? <p style={{ color: '#4C1' }}>{data}</p> :
+                <form onSubmit={handleSubmit}>
+                    <Input label="Email | User" type="text" name="email" {...login} />
+                    {loading ? <Button>Sending...</Button> : <Button>Send email</Button>}
+                </form>
+            }
+
+            <Error error={error} />
+        </section>
     )
 }
 
--- src/Components/Login/ResetPassword.js
+++ src/Components/Login/ResetPassword.js
@@ -1,9 +1,53 @@
 import React from 'react'
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
+import { PASSWORD_RESET } from '../../api';
+import useFetch from '../../Hooks/useFetch';
+import Error from '../Helper/Error';
+import { useNavigate } from 'react-router-dom';
 
 const ResetPassword = () => {
+    const [login, setLogin] = React.useState('');
+    const [key, setKey] = React.useState('');
+    const password = useForm();
+    const { error, loading, request } = useFetch();
+
+    const navigate = useNavigate();
+
+    React.useEffect(() => {
+        const params = new URLSearchParams(window.location.search);
+        const key = params.get('key');
+        const login = params.get('login');
+
+        if (key) setKey(key);
+        if (login) setLogin(login);
+    }, [])
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        if (password.validate()) {
+            const { url, options } = PASSWORD_RESET({
+                login,
+                key,
+                password: password.value
+            });
+
+            console.log(url)
+            console.log(options)
+            const { response } = await request(url, options);
+            console.log(response);
+            if (response.ok) navigate('/login')
+        }
+    }
     return (
         <div>
-            <h1>Reset password</h1>
+            <h1 className='title'>Reset your password</h1>
+            <form onSubmit={handleSubmit}>
+                <Input label="New password" type="password" name="password" {...password} />
+                {loading ? <Button disabled>Reseting...</Button> : <Button>Reset</Button>}
+            </form>
+            <Error error={error} />
         </div>
     )
 }
